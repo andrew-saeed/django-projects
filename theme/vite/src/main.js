e4018deb7a3d3967d2f306827909c88da196df74
andrew-saeed <andrew-saeed.dev@outlook.com>
2025-03-02 00:00:00 +0000

    convert page-layout into alpinejs component and use openLeftSide method to show aside.left-side

--- theme/vite/src/main.js
+++ theme/vite/src/main.js
@@ -1,1 +1,18 @@
-console.log('django 4')
+import Alpine from 'alpinejs'
+ 
+window.Alpine = Alpine
+
+document.addEventListener('alpine:init', () => {
+
+    const leftSide = document.querySelector('.left-side')
+    leftSide.style.transition = 'left 0.3s ease-out'
+
+    Alpine.data('layout', () => ({
+
+        openLeftSide() {
+            leftSide.style.left = '0'
+        },
+    }))
+})
+
+Alpine.start()
